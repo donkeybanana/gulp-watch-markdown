6d180d0f2b56ab1855d91e9ac5ca6b2a36af873c
Alan Burgoyne <alan@srcur.com>
2014-07-18 15:58:33 +0800

    Switch from gulp-watch to gaze to support directory watching, add primitive race condition prevention

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,40 @@
 var gulp = require('gulp')
-  , watch = require('gulp-watch')
-  , spawn = require('child_process').spawn;
+  , spawn = require('child_process').spawn
+  , gaze = require('gaze')
+  , util = require('gulp-util');
 
-gulp.task('default', function () {
-  gulp.src('./src/**/*.*')
-    .pipe(watch(function(files) {
+gulp.task('watch', function () {
+  var gaze = require('gaze')
+    , busy = false;
+
+  gaze('src/**/*', function(err, watcher) {
+    this.on('all', function(event, filepath) {
+      // Primitive race condition prevention,
+      // won't help the running process though...
+      if (busy) {
+        util.log(
+          'Waiting for previous task to complete,',
+          'stop hitting save'
+        );
+        return;
+      }
+      busy = true;
+
+      util.log(filepath.split(process.cwd())[1], 'was', event);
+      util.log('Running markdown-pp.py ...');
+
       process.chdir('src');
+
       var pp = spawn('markdown-pp.py', [
         'index.mdpp',
-        '../dist/README.md'
+        '../README.md'
       ]);
-      process.chdir('..');
-      return pp;
-    }));
+
+      pp.on('exit', function(){
+        util.log('Done');
+        busy = false;
+        process.chdir('..');
+      });
+    });
+  });
 });
